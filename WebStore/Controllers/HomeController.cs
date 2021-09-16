d6dcc0267958f193bbf829462bc8a251287b901f
ai_kapelyush <1>
2021-09-16 18:42:47 +0700

    Настройка middleware для ASP.NET Core MVC, создание модели, контроллера, предсатвлений

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly List<Employee> employees = new() 
+        {
+            new Employee { Id = 1, FirstName = "Иванов", LastName = "Иван", MiddleName="Иванович", Age = 33},
+            new Employee { Id = 2, FirstName = "Петров", LastName = "Максим", MiddleName = "Андреевич", Age = 22 },
+            new Employee { Id = 3, FirstName = "Потапенко", LastName = "Лариса", MiddleName = "Михайловна", Age = 18 }
+        };
+
+        /// <summary>
+        /// Главная страница (http://localhost:5000/Home/Index)
+        /// </summary>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Сотрудники (http://localhost:5000/Home/Employees)
+        /// </summary>
+        public IActionResult Employees()
+        {
+            return View(employees);
+        }
+    }
+}
