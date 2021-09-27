c092635e13b1c0484eb281f8825f196d96a39ed3
ai_kapelyush <1>
2021-09-27 12:51:48 +0700

    Реализация сервиса сотрудников

---
+++ WebStore/Services/InMemoryEmployeesData.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore.Data;
+using WebStore.Models;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Services
+{
+    public class InMemoryEmployeesData : IEmployeesData
+    {
+        private readonly ILogger<InMemoryEmployeesData> _logger;
+        private int _currentMaxId;
+        public InMemoryEmployeesData(ILogger<InMemoryEmployeesData> logger)
+        {
+            _logger = logger;
+            _currentMaxId = TestData.Employees.Max(e => e.Id);
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return TestData.Employees;
+        }
+
+        public Employee GetById(int id)
+        {
+            return TestData.Employees.FirstOrDefault(e => e.Id == id);
+        }
+
+        public int Add(Employee employee)
+        {
+            if (employee is null)
+                throw new ArgumentNullException(nameof(employee));
+
+            if (TestData.Employees.Contains(employee))
+                return employee.Id;
+
+            employee.Id = ++_currentMaxId;
+            TestData.Employees.Add(employee);
+
+            return employee.Id;
+        }
+
+        public void Update(Employee employee)
+        {
+            if (employee is null)
+                throw new ArgumentNullException(nameof(employee));
+
+            if (TestData.Employees.Contains(employee))
+                return; //Только для реализации сервиса в памяти
+
+            var db_employee = GetById(employee.Id);
+
+            if (db_employee == null)
+                return;
+
+            db_employee.FirstName = employee.FirstName;
+            db_employee.LastName = employee.LastName;
+            db_employee.MiddleName = employee.MiddleName;
+            db_employee.Age = employee.Age;
+
+            //db.SaveChanges();
+        }
+
+        public bool Delete(int id)
+        {
+            var db_employee = GetById(id);
+
+            if (db_employee == null) return false;
+
+            TestData.Employees.Remove(db_employee);
+
+            return true;
+        }
+    }
+}
