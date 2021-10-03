89dfab18506f3e75e46f26794d3c0bb6628c0328
ai_kapelyush <1>
2021-10-03 16:48:35 +0700

    Заготовки визуальных компонентов

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    //[ViewComponent(Name = "BrandsView")]
+    public class BrandsViewComponent: ViewComponent
+    {
+        //public async Task<IViewComponentResult> Invoke() => View();
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebStore/Components/CategoryViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    public class CategoryViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
