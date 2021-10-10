7a41eb4f72f06dd8b06e719af70c7d1f2989c1f4
ai_kapelyush <1>
2021-10-10 14:06:01 +0700

    Миграция БД

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebStore.Domain.Entities;
 
 namespace WebStore.DAL
 {
-    public class WebStoreDB: DbContext
+    /// <summary>
+    /// TODO: в идеале сделать два контекста (каталог товаров и пользователи и роли)
+    /// </summary>
+    public class WebStoreDB: IdentityDbContext //<User, Role, string> - собственные классы пользователя и роли, тип первичного ключа
     {
         //Описание свойств
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Brand> Brands { get; set; }
+
 
         public WebStoreDB(DbContextOptions<WebStoreDB> options): base(options)
         {
